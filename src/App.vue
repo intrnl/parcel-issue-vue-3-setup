f4eccb8b1e31dfdef8906a4c937759bf65a8c243
intrnl <intrnl28+git@gmail.com>
2021-09-17 11:02:32 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import { ref } from 'vue';
+
+	let count = ref(0);
+
+	function increment () {
+		count.value++;
+	}
+</script>
+
+<template>
+	<div class='red'>It's red!</div>
+	<button @click='increment'>{{ count }}</button>
+</template>
+
+<style scoped>
+	.red {
+		color: red;
+	}
+</style>
